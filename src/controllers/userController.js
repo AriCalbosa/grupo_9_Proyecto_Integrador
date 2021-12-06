48b46a950b8d2d3429b7bbcf1ce55f192315b873
AriCalbosa <ariel.l.calbosa@gmail.com>
2021-12-06 16:02:55 -0300

    Agrego base de datos de perfiles, vista del perfil del usuario con sus respectivos estilos, rutas y controladores

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
 const path = require('path');
+
+const rutaPerfiles = path.join(__dirname, '../data/perfiles.json');
+const perfiles = JSON.parse(fs.readFileSync(rutaPerfiles, 'utf-8'));
 
 const userController = {
     cuenta: (req, res) => {
         res.render('../src/views/users/register-login');
+    },
+    ingresar: (req, res) => {
+        
+    },
+    crear: (req, res) => {
+        
+    },
+    perfil: (req, res) => {
+        res.render('../src/views/users/profile');
     }
 };
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,24 @@
+// ************ Require's ************
 const express = require('express');
 const userRouter = express.Router();
 
+
+
+// ************ Controller Require ************
 const userController = require('../controllers/userController');
 
+
+
+/*** CUENTA ***/
 userRouter.get('/', userController.cuenta);
+userRouter.post('/perfil', userController.ingresar);
+userRouter.post('/perfil', userController.crear)
+
+
+
+/*** PERFIL ***/
+userRouter.get('/perfil', userController.perfil);
+
+
 
 module.exports = userRouter;
